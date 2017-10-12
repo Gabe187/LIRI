b40663be47e35570040df1d257ebc80a39a5815a
Gabe Solomon <Gabe@Gabes-MacBook-Pro.local>
2017-10-12 16:29:39 -0400

    Initial files

---
+++ keys.js
@@ -0,0 +1,12 @@
+// Keys for Twitter
+
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+module.exports = twitterKeys;
